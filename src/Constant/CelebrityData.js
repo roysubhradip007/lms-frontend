3faeac403e50b13f99df385d8b1a6b2588891196
paulSuman005 <sumanpauls568gmail.com>
2024-12-31 23:31:33 +0530

    Cleaned up our about us page

---
+++ src/Constant/CelebrityData.js
@@ -0,0 +1,39 @@
+import apj from '../Assets/Images/apj.png';
+import billGates from '../Assets/Images/billGates.png';
+import einstein from '../Assets/Images/einstein.png';
+import nelsonMandela from '../Assets/Images/nelsonMandela.png';
+import steveJobs from '../Assets/Images/steveJobs.png';
+
+
+export const celebrities = [
+    {
+        title: "Nelson Mandela",
+        description: "Education is the most powerful weapon which you can use to change the world.",
+        image: nelsonMandela,
+        slideNumber: 1
+    },
+    {
+        title: "A.P.J. Abdul Kalam",
+        description: "Failure will never overtake me if my determination to succeed is strong enough.",
+        image: apj,
+        slideNumber: 2
+    },
+    {
+        title: "Albert Einstein",
+        description: "Anyone who has never made a mistake has never tried anything new.",
+        image: einstein,
+        slideNumber: 3
+    },
+    {
+        title: "Steve Jobs",
+        description: "We're here to put a dent in the universe. Otherwise why else even be here?",
+        image: steveJobs,
+        slideNumber: 4
+    },
+    {
+        title: "Bill Gates",
+        description: "It’s fine to celebrate success, but it is more important to heed the lessons of failure.",
+        image: billGates,
+        slideNumber: 5
+    },
+]
